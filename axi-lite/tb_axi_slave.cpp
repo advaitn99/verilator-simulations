28f67aa2bbaf8c49ac375526e7e8680692c9a943
advaitn99 <advaitnerlikar@gmail.com>
2026-02-11 23:26:55 +0530

    added monitor

--- axi-lite/tb_axi_slave.cpp
+++ axi-lite/tb_axi_slave.cpp
@@ -5,7 +5,8 @@
 #include "Vaxi_slave.h"
 #include <random>
 #include <chrono>
-
+#include <thread>
+#include <fstream>
 
 #define MAX_SIM_TIME 2000
 #define NUM_TESTS    5
@@ -14,20 +15,6 @@ vluint64_t sim_time = 0;
 int num_test = 0;
 VerilatedVcdC *m_trace;
 Vaxi_slave *dut;
-
-
-// 1. Seed the random number engine
-// Use a time-based seed for different results each time the program runs
-unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-std::mt19937 engine(seed); // Mersenne Twister engine is a common choice
-
-// 2. Define the distribution
-// Creates a uniform distribution for integers between 1 and 100 (inclusive)
-std::uniform_int_distribution<int> dist_addr(1, 100);
-
-std::uniform_int_distribution<int> dist_data(1, 20);
-
-std::uniform_int_distribution<int> dist_op(0, 1);
 
 
 void tick(void)
@@ -52,18 +39,51 @@ class transaction
 };
 
 
+class RandomNumberGenerator {
+private:
+    std::mt19937 engine;
+    // The distribution is set when 'generate' is called, or you can store a specific one
+public:
+    RandomNumberGenerator() : engine(std::chrono::system_clock::now().time_since_epoch().count()) {
+        // Seed the engine using the current time
+    }
+
+    // Function to generate an integer within a specified range [min, max]
+    int generate_int(int min, int max) {
+        std::uniform_int_distribution<int> dist(min, max);
+        return dist(engine);
+    }
+};
+
+
 class generator
 {
     private:
         transaction *tx = new transaction();
+        RandomNumberGenerator rng;
 
     public:
         transaction* generate_sequence()
         {
-            tx->op     = rand() % 2;
-            tx->awaddr = rand() % 100;
-            tx->wdata  = rand() % 20;
-            tx->araddr = rand() % 100;
+            tx->op     = rng.generate_int(0,1);
+            tx->awaddr = rng.generate_int(0,100);
+            tx->wdata  = rng.generate_int(1,20);
+            tx->araddr = rng.generate_int(0,100);
+
+            std::ofstream logfile("sim_logs.log", std::ios_base::out | std::ios_base::app);
+
+            if (logfile.is_open()) 
+            {
+                logfile << "[GEN]: OP: " << (int)tx->op
+                        << " AWADDR: " << (int)tx->awaddr
+                        << " WDATA: " << (int)tx->wdata
+                        << " ARADDR: "<< (int)tx->araddr
+                <<std::endl;
+                logfile.close();
+            } else 
+            {
+                std::cerr << "Error: Unable to open log file." << std::endl;
+            }
 
             return tx;
         }
@@ -136,12 +156,6 @@ class driver
                 dut->s_wvalid = 0;
                 dut->s_wdata = 0;
                 dut->s_bready = 0;
-
-                while(dut->s_bvalid != 0)
-                {
-                    tick();
-                    tick();
-                }
             }
         }
 
@@ -184,6 +198,19 @@ class driver
 
         void drive(transaction *trans)
         {
+            std::ofstream logfile("sim_logs.log", std::ios_base::out | std::ios_base::app);
+            if (logfile.is_open()) 
+            {
+                logfile << "[DRV]: OP: " << (int)trans->op
+                        << " AWADDR: " << (int)trans->awaddr
+                        << " WDATA: " << (int)trans->wdata
+                        << " ARADDR: "<< (int)trans->araddr
+                <<std::endl;
+                logfile.close();
+            } else 
+            {
+                std::cerr << "Error: Unable to open log file." << std::endl;
+            }
             if(trans->op == 1)
             {
                 dut_write(trans);
@@ -193,6 +220,73 @@ class driver
                 dut_read(trans);
             }
         }
+};
+
+
+class  monitor
+{
+    private:
+        Vaxi_slave *dut;
+        transaction *t1 = new transaction();
+
+    public:
+        monitor(Vaxi_slave *dut)
+        {
+            this->dut = dut;
+        }
+
+        void monitor_dut(transaction *t2)
+        {
+            if(t2->op == 1)
+            {                
+                /* store write address */
+                t1->awaddr = t2->awaddr;
+                t1->wdata = t2->wdata;
+
+
+                t1->bresp = dut->s_bresp;
+                
+                std::ofstream logfile("sim_logs.log", std::ios_base::out | std::ios_base::app);
+                if (logfile.is_open()) 
+                {
+                    logfile << "[MON]: " << " BRESP: " << (int)t1->bresp <<std::endl;
+                    logfile.close();
+                } else 
+                {
+                    std::cerr << "Error: Unable to open log file." << std::endl;
+                }
+
+                while(dut->s_bvalid != 0)
+                {
+                    tick();
+                    tick();
+                }
+            }
+            else
+            {
+                t1->araddr = t2->araddr;
+
+                t1->rdata  = dut->s_rdata;
+                t1->rresp = dut->s_rresp;
+                std::ofstream logfile("sim_logs.log", std::ios_base::out | std::ios_base::app);
+                if (logfile.is_open()) 
+                {
+                    logfile << "[MON]: " << " RDATA: " << (int)t1->rdata
+                    << " RRESP: " << (int)t1->rresp <<std::endl;
+                    logfile.close();
+                } else 
+                {
+                    std::cerr << "Error: Unable to open log file." << std::endl;
+                }
+
+                while(dut->s_rvalid != 0)
+                {
+                    tick();
+                    tick();
+                }
+            }
+        }
+
 };
 
 
@@ -206,10 +300,20 @@ int main(int argc, char** argv, char** env) {
     transaction *t;
     generator *g = new generator();
     driver *d = new driver(dut);
+    monitor *m = new monitor(dut);
 
     dut->aclk = 0;
 
-    //std::cout<<"#### RESET DUT ####"<<std::endl;
+    std::ofstream logfile("sim_logs.log", std::ios_base::out | std::ios_base::app);
+
+    if (logfile.is_open()) 
+    {
+        logfile << "###### Reset DUT #######" << std::endl;
+        logfile.close();
+    } else 
+    {
+        std::cerr << "Error: Unable to open log file." << std::endl;
+    }
     d->dut_reset();
 
     while(num_test < NUM_TESTS)
@@ -222,17 +326,14 @@ int main(int argc, char** argv, char** env) {
         }
         tick();
 
-        //std::cout<<"#### GENERATE SEQUENCE  ####"<<std::endl;
         t = g->generate_sequence();
-        //std::cout<<"op: "<<t->op<<" awaddr: "<<t->awaddr<<" wdata: "<<t->wdata<<" araddr: "<<t->araddr<<std::endl;
 
         d->drive(t);
+        m->monitor_dut(t);
 
         num_test++;
     }
-
-    std::cout<<"NUM_TEST: "<<num_test<<std::endl;
-
+    
     m_trace->close();
     delete dut;
     exit(EXIT_SUCCESS);
